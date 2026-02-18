6724562221a51fa7f2713068b4ee4b610fe501ce
Checksum <me@checksum.space>
2026-02-18 12:59:49 -0600

    ci: I still hate clippy

--- nvidia-video-codec-sys/src/lib.rs
+++ nvidia-video-codec-sys/src/lib.rs
@@ -2,6 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::too_many_arguments)]
+#![allow(clippy::ptr_offset_with_cast)]
 #![allow(clippy::unnecessary_cast, clippy::useless_transmute)]
 #![allow(unnecessary_transmutes)]
 
