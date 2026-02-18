7e3b3a760f529cd35c6831cc7e0bc0b700452e02
Checksum <me@checksum.space>
2026-02-18 12:56:43 -0600

    ci: I hate clippy

--- nvidia-video-codec-sys/src/lib.rs
+++ nvidia-video-codec-sys/src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(clippy::unnecessary_cast, clippy::useless_transmute)]
+#![allow(unnecessary_transmutes)]
 
 mod bindings;
 pub use bindings::*;
