32bd8717d51d333a6c7c0a1761d42b8af8084994
Checksum <me@checksum.space>
2026-02-18 12:32:25 -0600

    fix: nvidia is hard.

--- nvidia-video-codec-sys/generate/wrapper.h
+++ nvidia-video-codec-sys/generate/wrapper.h
@@ -4,7 +4,7 @@
 // We deliberately exclude dynlink_loader.h because we provide our own Rust-native
 // dynamic loader (see src/loader.rs).
 
+#include "ffnvcodec/dynlink_cuda.h"
 #include "ffnvcodec/nvEncodeAPI.h"
 #include "ffnvcodec/dynlink_cuviddec.h"
 #include "ffnvcodec/dynlink_nvcuvid.h"
-#include "ffnvcodec/dynlink_cuda.h"
